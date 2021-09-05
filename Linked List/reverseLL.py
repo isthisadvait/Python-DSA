18b652c26913aa7bc3e9556c24db324445280073
Raman Mehta <86768225+isthisadvait@users.noreply.github.com>
2021-09-05 22:01:35 +0530

    Create reverseLL.py

---
+++ Linked List/reverseLL.py
@@ -0,0 +1,71 @@
+#Author Raman Mehta
+from sys import stdin, setrecursionlimit
+setrecursionlimit(10 ** 6)
+
+#Following is the Node class already written for the Linked List
+class Node :
+    def __init__(self, data) :
+        self.data = data
+        self.next = None
+
+
+
+
+def printReverse(head) :
+    #Your code goes here
+    if head==None:
+        return
+    if head.next==None:
+        print(head.data,end=" ")
+        return
+    printReverse(head.next)
+    print(head.data,end=" ")
+    
+    
+    
+    
+#Taking Input Using Fast I/O
+def takeInput() :
+    head = None
+    tail = None
+
+    datas = list(map(int, stdin.readline().rstrip().split(" ")))
+
+    i = 0
+    while (i < len(datas)) and (datas[i] != -1) :
+        data = datas[i]
+        newNode = Node(data)
+
+        if head is None :
+            head = newNode
+            tail = newNode
+
+        else :
+            tail.next = newNode
+            tail = newNode
+
+        i += 1
+
+    return head
+
+
+#to print the linked list 
+def printLinkedList(head) :
+
+    while head is not None :
+        print(head.data, end = " ")
+        head = head.next
+
+    print()
+
+
+#main
+t = int(stdin.readline().rstrip())
+
+while t > 0 :
+
+    head = takeInput()
+    printReverse(head)
+    print()
+
+    t -= 1
