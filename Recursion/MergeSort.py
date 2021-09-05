817e175f813543afb21b0a3869a340954ad01d49
Raman Mehta <86768225+isthisadvait@users.noreply.github.com>
2021-09-05 21:53:23 +0530

    Create MergeSort.py

---
+++ Recursion/MergeSort.py
@@ -0,0 +1,49 @@
+def MG(arr):
+    if len(arr) > 1:
+ 
+         # Finding the mid of the array
+        mid = len(arr)//2
+ 
+        # Dividing the array elements
+        L = arr[:mid]
+ 
+        # into 2 halves
+        R = arr[mid:]
+ 
+        # Sorting the first half
+        MG(L)
+ 
+        # Sorting the second half
+        MG(R)
+ 
+        i = j = k = 0
+ 
+        # Copy data to temp arrays L[] and R[]
+        while i < len(L) and j < len(R):
+            if L[i] < R[j]:
+                arr[k] = L[i]
+                i += 1
+            else:
+                arr[k] = R[j]
+                j += 1
+            k += 1
+ 
+        # Checking if any element was left
+        while i < len(L):
+            arr[k] = L[i]
+            i += 1
+            k += 1
+ 
+        while j < len(R):
+            arr[k] = R[j]
+            j += 1
+            k += 1
+    
+def mergeSort(arr, start, end):
+    # Please add your code here
+    MG(arr)
+# Main
+n=int(input())
+arr=list(int(i) for i in input().strip().split(' '))
+mergeSort(arr, 0, n)
+print(*arr)
