b4dc9e3842575331b4fe49bea46d0b43806cb08f
Raman Mehta <86768225+isthisadvait@users.noreply.github.com>
2021-09-05 21:51:37 +0530

    Create removeDup.py

---
+++ Recursion/removeDup.py
@@ -0,0 +1,15 @@
+# Problem ID 91, removeConsecutiveDuplicates
+def removeConsecutiveDuplicates(string):
+    # Please add your code here
+    if len(string)==1:
+        return string
+    if string[0]==string[1]:
+        return removeConsecutiveDuplicates(string[1:])
+    else:
+        return string[0]+removeConsecutiveDuplicates(string[1:])
+        
+        
+
+# Main
+string = input().strip()
+print(removeConsecutiveDuplicates(string))
